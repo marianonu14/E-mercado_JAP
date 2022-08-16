ff95fe0417b22cfc202b0d633a197c94ba3ecd77
Mariano <marianonunezma@gmail.com>
2022-08-16 10:33:25 -0300

    Car Products Section Ready

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productoContainer = document.getElementById('car-list-container');
+
+document.addEventListener('DOMContentLoaded',getProductData)
+
+async function getProductData(){
+    try {
+        const response = await fetch('https://japceibal.github.io/emercado-api/cats_products/101.json');
+        const result = await response.json();
+        showProduct(result.products);  
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function showProduct(products){
+    for(items of products){
+        productoContainer.innerHTML += `<div onclick="setCatID(${items.id})" class="list-group-item list-group-item-action cursor-active">
+        <div class="row">
+            <div class="col-3">
+                <img src="${items.image}" alt="${items.description}" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">${items.name}</h4>
+                    <small class="text-muted">${items.soldCount} vendidos</small>
+                </div>
+                <p class="mb-1">${items.description}</p>
+            </div>
+        </div>
+    </div>`
+    }
+    console.log(products);
+}
+
