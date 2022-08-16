45a4c8fcabb3450852a30f545ad31de315dd4307
Mariano <marianonunezma@gmail.com>
2022-08-16 12:11:16 -0300

    Form validation ready

--- js/login.js
+++ js/login.js
@@ -1,14 +1,17 @@
 const form = document.getElementById('form-container');
 const inputEmail = document.getElementById('email-input');
 const inputPassword = document.getElementById('password-input');
+const messageError = document.getElementById('message-error');
 
 form.addEventListener('submit', formSubmit);
 
 function validForm(){
     if(!inputEmail.value.includes('@')){
+        showMessage('Debe Ingresar un Mail Valido');
         return false;
     }
     if(inputPassword.value.length < 6){
+        showMessage('La Contraseña Debe Contener Mas de 6 Caracteres');
         return false;
     }
     return true;
@@ -22,3 +25,24 @@ function formSubmit(e){
         window.location = window.location = "index.html"
     } return;
 }
+
+function showMessage(message){
+    messageError.classList.remove('d-none')
+    messageError.classList.add('d-block')
+    messageError.textContent = message;
+    setTimeout(()=>{
+        messageError.classList.remove('d-block')
+        messageError.classList.add('d-none')
+    },1500)
+}
+
+
+
+
+
+
+
+
+
+
+
