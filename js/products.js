a3bcc7c1dd893b112fb157603ff47aebb22ff6f5
Mariano <marianonunezma@gmail.com>
2022-08-31 16:04:28 -0300

    Filter functions ready

--- js/products.js
+++ js/products.js
@@ -1,4 +1,19 @@
 const productoContainer = document.getElementById('car-list-container');
+const categoryName = document.getElementById('categoryName');
+
+const categories = {
+    '101': 'autos',
+    '102': 'juguetes',
+    '103': 'muebles',
+    '104': 'herramientas',
+    '105': 'computadoras',
+    '106': 'vestimenta',
+    '107': 'electrodomesticos',
+    '108': 'deporte',
+    '109': 'celulares'
+}
+
+let products = [];
 
 document.addEventListener('DOMContentLoaded',() => {
     const isLogin = localStorage.getItem('Auth');
@@ -6,7 +21,9 @@ document.addEventListener('DOMContentLoaded',() => {
     if(isLogin === 'false' || !isLogin){
         window.location = "/login.html"
     }
-    
+
+    categoryName.textContent= categories[localStorage.getItem("catID")]
+
     getProductData()})
 
 document.getElementById("logout").addEventListener("click", function() {
@@ -14,11 +31,14 @@ document.getElementById("logout").addEventListener("click", function() {
     window.location = "login.html"
 });
 
+// Fetch a los Productos Según su ID
+
 async function getProductData(){
     const categoriesId = localStorage.getItem('catID');
     try {
         const response = await fetch(`https://japceibal.github.io/emercado-api/cats_products/${categoriesId}.json`);
         const result = await response.json();
+        products = result.products;
         showProduct(result.products);  
     } catch (error) {
         console.log(error);
@@ -26,15 +46,17 @@ async function getProductData(){
 }
 
 function showProduct(products){
+    productoContainer.innerHTML = ''
+
     for(items of products){
-        productoContainer.innerHTML += `<div onclick="setCatID(${items.id})" class="list-group-item list-group-item-action cursor-active">
+        productoContainer.innerHTML += `<div onclick="console.log(${items.id})" class="list-group-item list-group-item-action cursor-active">
         <div class="row">
             <div class="col-3">
                 <img src="${items.image}" alt="${items.description}" class="img-thumbnail">
             </div>
             <div class="col">
                 <div class="d-flex w-100 justify-content-between">
-                    <h4 class="mb-1">${items.name}</h4>
+                    <h4 class="mb-1">${items.name} - ${items.currency} ${items.cost}</h4>
                     <small class="text-muted">${items.soldCount} vendidos</small>
                 </div>
                 <p class="mb-1">${items.description}</p>
@@ -43,3 +65,67 @@ function showProduct(products){
     </div>`
     }
 }
+
+//Filtrar por Precio y Articulos Vendidos
+
+document.getElementById("sortAsc").addEventListener("click", function(){
+    const productsAsc = products.sort((a, b) => {
+        if (a.cost > b.cost) {
+          return -1;
+        }
+        if (a.cost < b.cost) {
+          return 1;
+        }
+        return 0;
+      });
+      showProduct(productsAsc);
+});
+
+document.getElementById("sortDesc").addEventListener("click", function(){
+    const productsAsc = products.sort((a, b) => {
+        if (a.cost > b.cost) {
+          return 1;
+        }
+        if (a.cost < b.cost) {
+          return -1;
+        }
+        return 0;
+      });
+      showProduct(productsAsc);
+});
+
+document.getElementById("sortByCount").addEventListener("click", function(){
+    const productsAsc = products.sort((a, b) => {
+        if (a.soldCount > b.soldCount) {
+          return -1;
+        }
+        if (a.soldCount < b.soldCount) {
+          return 1;
+        }
+        return 0;
+      });
+      showProduct(productsAsc);
+});
+
+//Filtrar por Precio Max y Min por medio de los Inputs
+
+const inputMin = document.getElementById('rangeFilterCountMin');
+const inputMax = document.getElementById('rangeFilterCountMax');
+
+document.getElementById('rangeFilterCount').addEventListener('click', () =>{
+    valMin = inputMin.value || 0
+    valMax = inputMax.value || 0
+
+    if(valMin === 0 && valMax === 0) return;
+    
+    const arrayFilter = products.filter(item => item.cost > valMin && item.cost < valMax)
+    showProduct(arrayFilter);
+});
+
+document.getElementById('clearRangeFilter').addEventListener('click', () =>{
+    inputMin.value = ''
+    inputMax.value = ''
+    showProduct(products)
+});
+
+// Filtro en Tiempo Real
