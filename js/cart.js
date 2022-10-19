15a9fdf6349f8a675eeb61c70380639c8133d1e8
Mariano <marianonunezma@gmail.com>
2022-10-19 11:58:29 -0300

    Adding HTML Form - Cart Page && Delete Btn Items

--- js/cart.js
+++ js/cart.js
@@ -1,17 +1,18 @@
 const cartBody = document.getElementById('cart-body');
 
 document.addEventListener('DOMContentLoaded', () => {
-
   const arrayStorage = JSON.parse(localStorage.getItem('cart'));
 
-  if(arrayStorage) return showData(arrayStorage);
+  if (arrayStorage) return showData(arrayStorage);
 
   getData();
 });
 
 async function getData() {
   try {
-    const response = await fetch(`https://japceibal.github.io/emercado-api/user_cart/25801.json`);
+    const response = await fetch(
+      `https://japceibal.github.io/emercado-api/user_cart/25801.json`
+    );
     const result = await response.json();
     showData(result.articles);
   } catch (error) {
@@ -20,10 +21,9 @@ async function getData() {
 }
 
 function showData(articles) {
-
-  for(let item of articles) {
+  for (let item of articles) {
     const { id, name, currency, unitCost, image, count } = item;
-  
+
     cartBody.innerHTML += `
         <tr>
             <td><img class="thumbnail" src="${image}" alt="Img-Product"></td>
@@ -39,10 +39,11 @@ function showData(articles) {
                 min="1" 
                 />
             </td>
-            <td class="fw-bold">${currency} <span id="${id}-subtotal">${unitCost * 1}</span></td>
-            <!-- <td><button class="btn btn-danger" onClick="deleteProduct(${id})">Eliminar Producto</button></td> -->
+            <td class="fw-bold">${currency} <span id="${id}-subtotal">${
+      unitCost * 1
+    }</span></td>
+            <td><button class="btn btn-danger" onClick="deleteProduct(${id})">Eliminar</button></td>
         </tr>`;
-
   }
 }
 
@@ -53,18 +54,21 @@ function setQuantity(id, unitCost) {
   subTotalCurrent.textContent = inputCurrent.value * unitCost;
 }
 
-// function deleteProduct(id) {
-//   if (!confirm('¿Seguro Quieres Eliminar Este Producto del Carrito?')) return;
+function deleteProduct(id) {
+  if (!confirm('¿Seguro Quieres Eliminar Este Producto del Carrito?')) return;
 
-//   cartArray = cartArray.filter((item) => item.id !== id);
+  const arrayStorage = JSON.parse(localStorage.getItem('cart'));
 
-//   const arrayStorage = JSON.parse(localStorage.getItem('cart'));
-//   const filterArrayStorage = arrayStorage.filter((elem) => elem !== id);
-//   localStorage.setItem('cart', JSON.stringify(filterArrayStorage));
+  const filterArray = arrayStorage.filter((items) => items.id !== id);
 
-//   cartBody.innerHTML = '';
+  localStorage.setItem('cart', JSON.stringify(filterArray));
 
-//   if (!cartArray.length) return;
+  cartBody.innerHTML = '';
 
-//   showData(cartArray);
-// }
+  if (!filterArray.length) {
+    localStorage.removeItem('cart');
+    return getData();
+  }
+
+  showData(filterArray);
+}
