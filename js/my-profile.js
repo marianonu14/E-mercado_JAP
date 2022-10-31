54474776296b3409e41e4c44c7dced766f61d2ae
Mariano <marianonunezma@gmail.com>
2022-10-31 11:46:16 -0300

    Profile ready

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,4 +1,5 @@
 const forms = document.querySelectorAll('.needs-validation');
+const thumbnailProfile = document.getElementById('thumbnail-profile');
 const firstNameInput = document.getElementById('first-name');
 const secondNameInput = document.getElementById('second-name');
 const firstLastNameInput = document.getElementById('first-lastname');
@@ -8,21 +9,34 @@ const emailInput = document.getElementById('email-input');
 const numberInput = document.getElementById('telephone-number');
 
 document.addEventListener('DOMContentLoaded', () => {
-    const user = localStorage.getItem('userID');
-    const profileInfo = JSON.parse(localStorage.getItem('profileInfo'));
+  const user = localStorage.getItem('userID');
+  const profileInfo = JSON.parse(localStorage.getItem('profileInfo'));
 
-    emailInput.value = user
+  emailInput.value = user;
 
-    if(!profileInfo) return console.log('No hay nadie');
+  if (!profileInfo) return;
 
-    const { firstName, secondName , firstLastName , secondLastName, imgProfile, telephoneNumber} = profileInfo
+  const {
+    firstName,
+    secondName,
+    firstLastName,
+    secondLastName,
+    imgProfile,
+    telephoneNumber,
+  } = profileInfo;
 
-    firstNameInput.value= firstName
-    secondNameInput.value= secondName
-    firstLastNameInput.value= firstLastName
-    secondLastNameInput.value= secondLastName
-    numberInput.value= telephoneNumber
-})
+  if(imgProfile) {
+    const imageFile = (imgProfile.split("\\")[2])
+
+    thumbnailProfile.src = `./img/${imageFile}`
+  }
+
+  firstNameInput.value = firstName;
+  secondNameInput.value = secondName;
+  firstLastNameInput.value = firstLastName;
+  secondLastNameInput.value = secondLastName;
+  numberInput.value = telephoneNumber;
+});
 
 // Loop over them and prevent submission
 Array.prototype.slice.call(forms).forEach(function (form) {
@@ -33,10 +47,9 @@ Array.prototype.slice.call(forms).forEach(function (form) {
         event.preventDefault();
         event.stopPropagation();
       }
-      if(form.checkValidity()){
-          saveInformation()
+      if (form.checkValidity()) {
+        saveInformation();
       }
-      event.preventDefault();
       form.classList.add('was-validated');
     },
     false
@@ -44,15 +57,15 @@ Array.prototype.slice.call(forms).forEach(function (form) {
 });
 
 function saveInformation() {
-    const profileInfo ={
-        firstName: firstNameInput.value,
-        secondName: secondNameInput.value,
-        firstLastName: firstLastNameInput.value,
-        secondLastName: secondLastNameInput.value,
-        imgProfile: imageInput.value,
-        email: emailInput.value,
-        telephoneNumber: numberInput.value
-    }
+  const profileInfo = {
+    firstName: firstNameInput.value,
+    secondName: secondNameInput.value,
+    firstLastName: firstLastNameInput.value,
+    secondLastName: secondLastNameInput.value,
+    imgProfile: imageInput.value,
+    email: emailInput.value,
+    telephoneNumber: numberInput.value,
+  };
 
-    localStorage.setItem('profileInfo', JSON.stringify(profileInfo));
+  localStorage.setItem('profileInfo', JSON.stringify(profileInfo));
 }
